d4f342a5441f897698816c6b2227f1b200e6525a
NaTy <natrod141@gmail.com>
2026-05-06 19:25:44 -0300

    fix: refefinição feminino p female

--- app/app.py
+++ app/app.py
@@ -83,7 +83,7 @@ taxa_sobrevivencia = df_filtrado['sobreviveu'].mean() * 100
 total_passageiros = len(df_filtrado)
 media_idade = df_filtrado['idade'].mean()
 
-df_feminino = df_filtrado[df_filtrado['sexo'] == 'feminino']
+df_feminino = df_filtrado[df_filtrado['sexo'] == 'female']
 taxa_feminina = df_feminino['sobreviveu'].mean() * 100 if len(df_feminino) > 0 else 0
 
 # Exibição dos KPIs
